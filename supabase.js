f57954b31f18b90dfa3c2bc734209a59c725b4f6
Totti <toti.clash9@gmail.com>
2026-03-01 23:56:04 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://qdvywfdfalzgjjxigsfv.supabase.co";
+  const SUPABASE_ANON_KEY = "sb_publishable_7Ui7z4RjONohMUMFwXb7mg_P4BzzONL";
+
+  const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+</script>
